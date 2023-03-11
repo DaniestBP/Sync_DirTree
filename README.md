98633f71b226c4922dc697928d36c0b57e712dab
DaniestBP <dani.ballesterosponcet@gmail.com>
2023-03-11 21:17:32 +0000

    final commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Recursive directories sync script :
+
+Program that synchronizes two folders: source and replica.
+- One-way synchronization: after the synchronization, content of the replica folder is modified to match source´s content.
+- Synchronization is performed periodically in hours, mins, or seconds as the user requires/specifies.
+- File creation/copying/removal operations are logged to a file and displayed on the console output;
+- Folder paths, synchronization interval and log file path must be provided using the command line arguments;
+
+## How it works:
+
+1. Clone repository from Github: 
+$ https://github.com/DaniestBP/Sync_DirTree.git
+
+2. Create a Virtual Enviroment to run the script: python3 -m venv .venv
+
+3. Activate env: source venv/bin/activate (in Linux) / source venv/Scripts/activate (in Windows)
+
+4. Install requirements: 
+
+$ pip3 install -r requirements.txt
+
+5. Execute program: python3 re_sync.py -src (source file abs path) -dst (replica file abs path) -sec (in case you want set sync.up in seconds) -min (same in minutes) -hours (samething)
+NOTE: Sync. may be set as a summatory of hours, minutes and seconds  -->
+
+6. Whenever you want to stop program use: Ctrl+C
+
--- re_sync.py
+++ re_sync.py
@@ -2,8 +2,25 @@ import os
 import argparse
 import shutil
 import hashlib
+import logging
+import schedule
+import time
+import sys
 
 
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+logFormatter = logging.Formatter("%(asctime)s %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format(os.getcwd(), "modifications"))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+  
 class FileSync():
 
     def _recurse(self, parent_path, file_list, sync_path):
@@ -14,7 +31,7 @@ class FileSync():
             return
 
         if not os.path.exists(sync_path):
-            print("NEW FOLDER: ", sync_path)
+            logging.info(f"New Folder: {sync_path}")
             os.mkdir(sync_path)
 
         for sub_path in file_list:
@@ -25,7 +42,7 @@ class FileSync():
                 sync_full_path = os.path.join(sync_path, sub_path)
 
                 if not os.path.exists(sync_full_path):
-                    print("NEW FOLDER: ", sync_full_path)
+                    logging.info(f"New Folder: {sync_full_path}")
                     os.mkdir(sync_full_path)
 
                 self._recurse(full_path, os.listdir(full_path), sync_full_path)
@@ -37,11 +54,13 @@ class FileSync():
 
                 if not (os.path.exists(sync_file)):
                     shutil.copy2(full_path, sync_path)
-                    print("NEW FILE: ", sync_file)
+                    logging.info(f"New file: {sync_file}")
+            
 
                 elif not self._check_md5(full_path, sync_file):
                     shutil.copy2(full_path, sync_path)
-                    print("UPDATE FILE: ", sync_file)
+                    logging.info(f"Updated file: {sync_file}")
+                    
 
     def _check_md5(self, input_file, output_file):
         with open(input_file, "r", encoding="utf-8", errors="ignore") as in_file, \
@@ -54,7 +73,7 @@ class FileSync():
     def _delete_recurse(self, folder, subdirs):
         if len(subdirs) == 0:
             os.rmdir(folder)
-            print("DELETED FOLDER", folder)
+            logging.info(f"Deleted folder: {folder}")
             return
 
         for file in subdirs:
@@ -64,10 +83,12 @@ class FileSync():
                 self._delete_recurse(full_path, os.listdir(full_path))
                 if os.path.exists(full_path):
                     os.rmdir(full_path)
-                    print("DELETED FOLDER", full_path)
+                    logging.info(f"Deleted Folder: {full_path}")
+                    
             else:
                 os.remove(full_path)
-                print("DELETED FILE", full_path)
+                logging.info(f"Deleted File: {full_path}")
+                
 
     def _deleted(self, file_list, sync_path):
         src_files = set(file_list)
@@ -84,16 +105,17 @@ class FileSync():
                     self._delete_recurse(full_path, os.listdir(full_path))
                 else:
                     os.remove(full_path)
-                    print("DELETE FILE: ", full_path)
+                    logging.info(f"Deleted File: {full_path}")
+                    
 
     def sync(self, args):
         self.src = args.source
         self.dst = args.destination
-
+       
         if not os.path.isdir(self.dst):
             os.mkdir(self.dst)
-            
         self._recurse(self.src, os.listdir(self.src), self.dst)
+            
 
 
 if __name__ == "__main__":
@@ -103,7 +125,20 @@ if __name__ == "__main__":
                         help="Original folder", default=".")
     parser.add_argument("-dst", "--destination",
                         help="Destination folder", default="../folderSync")
+    parser.add_argument("-sec", "--seconds",
+                        help="Time in seconds to sync folders", type=int, default=0)
+    parser.add_argument("-min", "--minutes",
+                        help="Time in minutes to sync folders", type=int, default=0)
+    parser.add_argument("-hrs", "--hours",
+                        help="Time in hours to sync folders", type=int, default=0)
+    
 
     args = parser.parse_args()
-    FileSync().sync(args)
+    frecuency= args.hours*3600 + args.minutes*60 + args.seconds
 
+    schedule.every(frecuency).seconds.do(FileSync().sync, args=args)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
